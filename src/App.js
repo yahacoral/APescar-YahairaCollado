86bee8f58ec2966a8951bf9282e805532a0402b8
yahacoral <yahairavittoria@gmail.com>
2022-03-29 15:56:55 -0500

    Add navbar component with material ui library

--- src/App.js
+++ src/App.js
@@ -1,25 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Banner from "./Components/Banner"
+import Navbar from "./Components/Navbar";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Creating new react app
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <>
+    <Banner/>
+    <Navbar/>
+    </>
   );
 }
-
-export default App;
