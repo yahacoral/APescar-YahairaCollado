8731d1ce4d4a82ff2c9a294b81d5a732a234490d
yahacoral <yahairavittoria@gmail.com>
2022-04-14 21:08:32 -0500

    Add React Router

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { BrowserRouter as Router } from "react-router-dom";
 import Banner from "./Components/Banner";
 import Navbar from "./Components/Navbar";
 import Mainview from "./Components/Mainview";
@@ -6,10 +7,12 @@ import "./App.css";
 
 export default function App() {
   return (
-  <>
-    <Banner/>
-    <Navbar/>
-    <Mainview/>
-  </>
+    <Router>
+      <div className="App">
+        <Banner/>
+        <Navbar/>
+        <Mainview/>
+      </div>
+    </Router>
   );
 }
